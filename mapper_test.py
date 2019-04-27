9b6f3a40ae99406e1a927850fece7280e86f1993
ignaciojnr <38479182+ignaciojnr@users.noreply.github.com>
2019-04-27 00:11:06 -0400

    archivos agregados

---
+++ mapper_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# Format of each line is:
+# date\ttime\tstore name\titem description\tcost\tmethod of payment
+#
+# We want elements 2 (store name) and 4 (cost)
+# We need to write them out to standard output, separated by a tab
+
+import sys
+
+for line in sys.stdin:
+    data = line.strip().split(",")
+    if len(data) == 3:
+	continue
+    print "{0},{1}".format(data[2], data[4])
+
